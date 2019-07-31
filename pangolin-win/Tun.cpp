d5b7d41c867cd347f0d8a328aac77d71a91c34d8
xitongsys <xitongsys@gmail.com>
2019-07-31 16:23:09 +0800

    del queue

--- pangolin-win/Tun.cpp
+++ pangolin-win/Tun.cpp
@@ -29,27 +29,60 @@ Tun::~Tun(){}
 
 vector<uint8_t> Tun::read() {
 	vector<uint8_t> res;
-	readQueue.try_pop(res);
+	Frame frame;
+	struct pcap_pkthdr* header;
+	const u_char* data;
+	if (pcap_next_ex(adhandle, &header, &data) <= 0) {
+		return res;
+	}
+
+	int rn = frame.read((uint8_t*)data, header->caplen);
+	if (rn < 0) return res;
+
+	int b = frame.ethernet.header_length();
+	for (int i = b; i < header->caplen; i++) {
+		res.push_back(data[i]);
+	}
+
 	return res;
 }
 
 bool Tun::write(vector<uint8_t>& data) {
-	if (writeQueue.unsafe_size() > BUFFSIZE) return false;
-	writeQueue.push(data);
+	Frame frame;
+	struct pcap_pkthdr* header;
+	uint8_t buf[6553];
+
+	if (data.size() <= 0) return true;
+
+	Ethernet ethernet;
+	ethernet.type = 0x0800;
+	ethernet.dst[0] = 0xff;
+	ethernet.dst[1] = 0xff;
+	ethernet.dst[2] = 0xff;
+	ethernet.dst[3] = 0xff;
+	ethernet.dst[4] = 0xff;
+	ethernet.dst[5] = 0xff;
+
+	ethernet.src[0] = 0xff;
+	ethernet.src[1] = 0xff;
+	ethernet.src[2] = 0xff;
+	ethernet.src[3] = 0xff;
+	ethernet.src[4] = 0xff;
+	ethernet.src[5] = 0xff;
+
+	int wn = ethernet.write(buf, 6553);
+	if (wn < 0) {
+		return false;
+	}
+
+	for (int i = 0; i < data.size(); i++, wn++) {
+		buf[wn] = data[i];
+	}
+
+	pcap_sendpacket(adhandle, buf, wn);
 	return true;
 }
 
-vector<uint8_t> Tun::_read() {
-	vector<uint8_t> res;
-	writeQueue.try_pop(res);
-	return res;
-}
-
-bool Tun::_write(vector<uint8_t>& data) {
-	if (readQueue.unsafe_size() > BUFFSIZE) return false;
-	readQueue.push(data);
-	return true;
-}
 
 bool Tun::start() {
 	if (adhandle != NULL) return false;
@@ -63,62 +96,6 @@ bool Tun::start() {
 		errbuf)) == NULL) {
 		return false;
 	}
-
-	read_thread = new thread(&Tun::packet_read, this);
-	write_thread = new thread(&Tun::packet_write, this);
 	return true;
 }
 
-void Tun::packet_read() {
-	Frame frame;
-	struct pcap_pkthdr* header;
-	const u_char* data;
-	while (true) {
-		if (pcap_next_ex(adhandle, &header, &data) <= 0) continue;
-		int rn = frame.read((uint8_t*)data, header->caplen);
-		if (rn < 0) return;
-
-		int b = frame.ethernet.header_length();
-		vector<uint8_t> vs;
-		for (int i = b; i < header->caplen; i++) {
-			vs.push_back(data[i]);
-		}
-		_write(vs);
-	}
-}
-
-void Tun::packet_write() {
-	Frame frame;
-	struct pcap_pkthdr* header;
-	const u_char* data;
-	uint8_t buf[6553];
-
-	while (true) {
-		vector<uint8_t> vs = _read();
-		if (vs.size() <= 0) continue;
-		
-		Ethernet ethernet;
-		ethernet.type = 0x0800;
-		ethernet.dst[0] = 0xff;
-		ethernet.dst[1] = 0xff;
-		ethernet.dst[2] = 0xff;
-		ethernet.dst[3] = 0xff;
-		ethernet.dst[4] = 0xff;
-		ethernet.dst[5] = 0xff;
-
-		ethernet.src[0] = 0xff;
-		ethernet.src[1] = 0xff;
-		ethernet.src[2] = 0xff;
-		ethernet.src[3] = 0xff;
-		ethernet.src[4] = 0xff;
-		ethernet.src[5] = 0xff;
-
-		int wn = ethernet.write(buf, 6553);
-		if (wn < 0) continue;
-		for (int i = 0; i < vs.size(); i++, wn++) {
-			buf[wn] = vs[i];
-		}
-
-		pcap_sendpacket(adhandle, buf, wn);
-	}
-}
--- pangolin-win/udp_client.cpp
+++ pangolin-win/udp_client.cpp
@@ -12,20 +12,35 @@ Udp_client::~Udp_client() {
 }
 
 bool Udp_client::start() {
+	WORD socketVersion = MAKEWORD(2, 2);
+	WSADATA wsaData;
+	if (WSAStartup(socketVersion, &wsaData) != 0) {
+		return false;
+	}
+
 	struct sockaddr_in server_info;
 	int len = sizeof(server_info);
+	memset(&server_info, 0, len);
 	server_info.sin_family = AF_INET;
-	server_info.sin_port = config->server_port;
-	server_info.sin_addr.s_addr = inet_addr(config->server_ip.c_str());
+	server_info.sin_port = htons(config->server_port);
+	inet_pton(AF_INET, config->server_ip.c_str(), (void*)& server_info.sin_addr.S_un.S_addr);
 
-	SOCKET sk = socket(AF_INET, SOCK_DGRAM, 0);
+	SOCKET sk = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (sk == SOCKET_ERROR) {
+		return false;
+	}
+
 	u_long mode = 1;
 	ioctlsocket(sk, FIONBIO, &mode);
 	char buf[65536];
 	while (true) {
 		vector<uint8_t> data = tun->read();
 		if (data.size() > 0) {
-			sendto(sk, buf, data.size(), 0, (sockaddr*)& server_info, len);
+			for (int i = 0; i < data.size(); i++) {
+				buf[i] = data[i];
+			}
+			sendto(sk, buf, data.size(), 0, (sockaddr*)& server_info, sizeof(sockaddr));
+			cout << "send  "<<data.size() << endl;
 		}
 
 		if (recvfrom(sk, buf, sizeof(buf), 0, (sockaddr*)& server_info, &len) != SOCKET_ERROR) {
@@ -33,7 +48,7 @@ bool Udp_client::start() {
 			for (int i = 0; i < len; i++) {
 				data.push_back(buf[i]);
 			}
-			cout << data.size() << endl;
+			cout << "recv  " << data.size() << endl;
 			tun->write(data);
 		}
 	}
