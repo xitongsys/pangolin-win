6e26602a612e0535bcd8de74669c0f4a79418f0a
xitongsys <xitongsys@gmail.com>
2019-08-09 10:28:29 +0800

    adding ui

---
+++ pangolin-ui/Config.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace pangolin_ui
+{
+    [DataContract]
+    public class Config
+    {
+        [DataMember]
+        public string server;
+        [DataMember]
+        public string dns;
+        [DataMember]
+        public string tun;
+        [DataMember]
+        public string protocol;
+        [DataMember]
+        public int mtu;
+        [DataMember]
+        public ArrayList tokens = new ArrayList();
+
+        public Config()
+        {
+            server = "0.0.0.0:12345";
+            dns = "1.1.1.1";
+            tun = "10.0.0.22";
+            protocol = "PTCP";
+            mtu = 1400;
+            tokens.Add("token01");
+        }
+
+        public void ReadConfig()
+        {
+            string cs = System.IO.File.ReadAllText("config.txt");
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(cs));
+            var ser = new DataContractJsonSerializer(this.GetType());
+            var desConfig = new Config();
+            desConfig = ser.ReadObject(ms) as Config;
+            ms.Close();
+            Assign(desConfig);
+        }
+
+        public void SaveConfig()
+        {
+            var ms = new MemoryStream();
+            var ser = new DataContractJsonSerializer(this.GetType());
+            ser.WriteObject(ms, this);
+            ms.Position = 0;
+            var sr = new StreamReader(ms);
+            System.IO.File.WriteAllText("config.txt", sr.ReadToEnd());
+        }
+
+        public void Assign(Config c)
+        {
+            server = c.server;
+            dns = c.dns;
+            tun = c.tun;
+            protocol = c.protocol;
+            mtu = c.mtu;
+            tokens = c.tokens;
+        }
+    }
+}
---
+++ pangolin-ui/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pangolin_ui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Config cfg;
+        public MainWindow()
+        {
+            InitializeComponent();
+            cfg = new Config();
+            try
+            {
+                cfg.ReadConfig();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Can't open config.txt: " + e.ToString());
+
+            }
+
+            LoadConfigToUI();
+        }
+
+        public void LoadConfigToUI()
+        {
+            serverEdit.Text = cfg.server;
+            dnsEdit.Text = cfg.dns;
+            tunEdit.Text = cfg.tun;
+            mtuEdit.Text = cfg.mtu.ToString();
+            if(cfg.protocol.ToUpper() == "PTCP")
+            {
+                protoComboBox.SelectedItem = 0;
+            }
+            else
+            {
+                protoComboBox.SelectedItem = 1;
+            }
+            if(cfg.tokens.Count > 0)
+            {
+                tokenEdit.Text = cfg.tokens[0].ToString();
+            }
+        }
+
+        public void LoadUItoConfig()
+        {
+            cfg.server = serverEdit.Text;
+            cfg.dns = dnsEdit.Text;
+            cfg.tun = tunEdit.Text;
+            cfg.mtu = Int32.Parse(mtuEdit.Text);
+            cfg.tokens.Clear();
+            cfg.tokens.Add(tokenEdit.Text);
+            cfg.protocol = ((ComboBoxItem)protoComboBox.SelectedItem).Content.ToString();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadUItoConfig();
+            cfg.SaveConfig();
+        }
+    }
+}
